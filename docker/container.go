66ce2578f23f9118cb1d062fe00b711ecf54006b
Nathan Osman <nathan@quickmediasolutions.com>
2017-07-14 14:34:55 -0700

    Add Container type.

---
+++ docker/container.go
@@ -0,0 +1,46 @@
+package docker
+
+import (
+	"strings"
+
+	"github.com/docker/docker/api/types"
+)
+
+const (
+	labelAddr    = "caddy.addr"
+	labelDomains = "caddy.domains"
+)
+
+// Container contains information about a running container. This includes both
+// information from Docker (its ID, etc.) and information from labels (domains,
+// address, etc.).
+type Container struct {
+	ID      string
+	Name    string
+	Domains []string
+	Addr    string
+}
+
+// containerFromJSON reads information from a container's labels and populates
+// a Container instance with the data. If the container does not contain the
+// requires labels, nil is returned.
+func containerFromJSON(json types.ContainerJSON) *Container {
+	domainRaw, ok := json.Config.Labels[labelDomains]
+	if !ok {
+		return nil, nil
+	}
+	domains := []string{}
+	for _, d := range strings.Split(domainRaw, ",") {
+		domains = append(domains, strings.TrimSpace(d))
+	}
+	addr, ok := json.Config.Labels[labelAddr]
+	if !ok {
+		return nil, nil
+	}
+	return &Container{
+		ID:      json.ID,
+		Name:    json.Name,
+		Domains: domains,
+		Addr:    addr,
+	}
+}
